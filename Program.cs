99f1853644d95cb429e01bbfdd2e27e61f6892db
Nathan Barnes <339723215@GoTVDSB.ca>
2024-10-09 09:38:28 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace T7_Nate_s_casino_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int bet;
+
+
+
+
+
+            Console.WriteLine("Hello user, welcome to nates casino toin coss. How much money would you like to bet");
+            Console.ReadLine
+
+
+
+
+
+
+        }
+    }
+}
