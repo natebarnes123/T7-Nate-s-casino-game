7fd84bdcb263b400617f4951aeb4da8e9a1671e9
Nathan Barnes <339723215@GoTVDSB.ca>
2024-10-10 09:38:22 -0400

    ryutryutryu

--- Program.cs
+++ Program.cs
@@ -5,13 +5,17 @@
         static void Main(string[] args)
         {
             int bet;
+            string answer;
+            int points = 3;
 
 
 
+            Console.WriteLine("Hello user, welcome to nates casino toin coss. How much points would you like to bet, you have 3 total right now.");
+            points = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Will you pick heads or tails?");
 
 
-            Console.WriteLine("Hello user, welcome to nates casino toin coss. How much money would you like to bet");
-            Console.ReadLine
 
 
 
