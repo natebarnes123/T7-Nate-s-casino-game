b2689e908eaf0f7ed165debe50bfaaee27b0b1c8
Nathan Barnes <339723215@GoTVDSB.ca>
2024-10-16 09:40:22 -0400

    gdfdf

--- Program.cs
+++ Program.cs
@@ -6,26 +6,45 @@ namespace T7_Nate_s_casino_game
     {
         static void Main(string[] args)
         {
-            int bet;
-            string answer;
+            string userChoice, playAgain;
             int points = 3;
             bool done = false;
             Random generator = new Random();
             int headOrTail;
-            
 
+            // 0 - Heads
+            // 1 - Tails
+
+            string heads, tails;
 
             Console.WriteLine("Hello user, welcome to nates casino toin coss.");
             while (!done)
             {
-                Console.WriteLine("Would you like to pick heads or tails");
-
                 
 
+                Console.Write("Would you like to pick heads or tails: ");
+                userChoice = Console.ReadLine().ToLower();
+               
+                headOrTail = generator.Next(2);
 
+
+                if (headOrTail == 0)
+                {
+                    Console.WriteLine("The computer flipped a heads");
+                }
+                else
+                {
+                    Console.WriteLine("The computer flipped a tails");
+                }
+               
+
+                Console.WriteLine("Do you want to play again?");
+
+                playAgain = Console.ReadLine().ToLower();
+                if (playAgain == "n" || playAgain == "no") 
+                    done = true;
             }
 
-            Console.Write(generator.Next(1) + " ");
 
 
 
